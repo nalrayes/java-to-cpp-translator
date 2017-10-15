fc8af96bda1538de9e7d6a3922c2ed0040b292d9
charleswdickstein <charliewoodrowdickstein@gmail.com>
2017-10-15 16:19:02 -0400

    outline of intermediate data structure

---
+++ src/main/java/edu/nyu/oop/IntermediateTree.java
@@ -0,0 +1,56 @@
+package edu.nyu.oop;
+import java.util.*;
+
+import scala.collection.immutable.Stream;
+import xtc.tree.Node;
+
+// this file outlines the intermediate sdata strucutre
+
+// an array list of classes
+
+class ClassObject{
+
+    // an arraylist of methods
+    ArrayList<Method> methods;
+
+    ArrayList<Stream.Cons> constructors;
+
+
+    ArrayList<LocalVariable> localVariables;
+
+    ArrayList<String> modifiers;
+
+
+
+ }
+
+
+ class Method{
+
+        String visibility;
+        String modifier;
+        ArrayList<Object> parameters;
+        String returnType;
+
+
+
+ }
+
+class Constructor{
+
+    String visibility;
+    ArrayList<Object> parameters;
+
+
+
+
+}
+
+class LocalVariable{
+
+
+    String visibility;
+    String name;
+    String type;
+
+}
