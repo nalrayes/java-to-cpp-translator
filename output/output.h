3ef6886efb38dbb267a7138bcb42d97fa2531d0b
charleswdickstein <charliewoodrowdickstein@gmail.com>
2017-12-05 12:49:04 -0500

    started translating block

--- output/output.h
+++ output/output.h
@@ -5,7 +5,7 @@
 using namespace java::lang;
 
 namespace inputs {
-  namespace test041 {
+  namespace CustomTests {
   
     struct __A;
     struct __A_VT;
@@ -17,9 +17,11 @@ namespace inputs {
       
       __A();
 
-      static void m_Object_Object(A,Object,Object);
-      static void m_A_Object(A,A,Object);
-      static void m_Object_A(A,Object,A);
+      String a;
+
+      static void setA_String(A,String);
+      static void printOther_A(A,A);
+      static String toString(A);
       static A __init();
       static Class __class();
 
@@ -31,66 +33,107 @@ namespace inputs {
 
       int32_t (*hashCode) (A);
       Class (*getClass) (A);
-      String (*toString) (A);
       bool (*equals) (A, Object);
-      void (*m_Object_Object) (A);
-      void (*m_A_Object) (A);
-      void (*m_Object_A) (A);
+      void (*setA_String) (A);
+      void (*printOther_A) (A);
+      String (*toString) (A);
 
       __A_VT()
       : __is_a(__A::__class()),
         hashCode((int32_t (*)(A)) &__Object::hashCode),
         getClass((Class (*)(A)) &__Object::getClass),
-        toString((String (*)(A)) &__Object::toString),
         equals((bool (*)(A, Object)) &__Object::equals),
-        m_Object_Object(&__A::m_Object_Object),
-        m_A_Object(&__A::m_A_Object),
-        m_Object_A(&__A::m_Object_A) {}
+        setA_String(&__A::setA_String),
+        printOther_A(&__A::printOther_A),
+        toString(&__A::toString) {}
     };
 
-    struct __B;
-    struct __B_VT;
-    typedef __B* B;
+    struct __B1;
+    struct __B1_VT;
+    typedef __B1* B1;
     
-    struct __B { 
+    struct __B1 { 
       
-      __B_VT* __vptr;
+      __B1_VT* __vptr;
       
-      __B();
+      __B1();
 
-      static void m_Object_Object(B,Object,Object);
-      static void m_B_Object(B,B,Object);
-      static void m_Object_B(B,Object,B);
-      static B __init();
+      String b;
+
+      static void helloWorld(B1);
+      static B1 __init();
       static Class __class();
 
-      static __B_VT __vtable;
+      static __B1_VT __vtable;
     };
 
-    struct __B_VT { 
+    struct __B1_VT { 
       Class __is_a;
 
-      void (*m_A_Object) (B);
-      void (*m_Object_A) (B);
-      int32_t (*hashCode) (B);
-      Class (*getClass) (B);
-      String (*toString) (B);
-      bool (*equals) (B, Object);
-      void (*m_B_Object) (B);
-      void (*m_Object_B) (B);
-      void (*m_Object_Object) (B);
+      void (*setA_String) (B1);
+      void (*printOther_A) (B1);
+      String (*toString) (B1);
+      int32_t (*hashCode) (B1);
+      Class (*getClass) (B1);
+      bool (*equals) (B1, Object);
+      void (*helloWorld) (B1);
 
-      __B_VT()
-      : __is_a(__B::__class()),
-        m_A_Object((void (*)(B)) &__A::m_A_Object),
-        m_Object_A((void (*)(B)) &__A::m_Object_A),
-        hashCode((int32_t (*)(B)) &__Object::hashCode),
-        getClass((Class (*)(B)) &__Object::getClass),
-        toString((String (*)(B)) &__Object::toString),
-        equals((bool (*)(B, Object)) &__Object::equals),
-        m_B_Object(&__B::m_B_Object),
-        m_Object_B(&__B::m_Object_B),
-        m_Object_Object(&__B::m_Object_Object) {}
+      __B1_VT()
+      : __is_a(__B1::__class()),
+        setA_String((void (*)(B1)) &__A::setA_String),
+        printOther_A((void (*)(B1)) &__A::printOther_A),
+        toString((String (*)(B1)) &__A::toString),
+        hashCode((int32_t (*)(B1)) &__Object::hashCode),
+        getClass((Class (*)(B1)) &__Object::getClass),
+        equals((bool (*)(B1, Object)) &__Object::equals),
+        helloWorld(&__B1::helloWorld) {}
+    };
+
+    struct __B2;
+    struct __B2_VT;
+    typedef __B2* B2;
+    
+    struct __B2 { 
+      
+      __B2_VT* __vptr;
+      
+      __B2();
+
+      String b;
+
+      static void goodbye(B2);
+      static void helloWorld(B2);
+      static void helloJohn(B2);
+      static B2 __init();
+      static Class __class();
+
+      static __B2_VT __vtable;
+    };
+
+    struct __B2_VT { 
+      Class __is_a;
+
+      void (*setA_String) (B2);
+      void (*printOther_A) (B2);
+      String (*toString) (B2);
+      int32_t (*hashCode) (B2);
+      Class (*getClass) (B2);
+      bool (*equals) (B2, Object);
+      void (*goodbye) (B2);
+      void (*helloJohn) (B2);
+      void (*helloWorld) (B2);
+
+      __B2_VT()
+      : __is_a(__B2::__class()),
+        setA_String((void (*)(B2)) &__A::setA_String),
+        printOther_A((void (*)(B2)) &__A::printOther_A),
+        toString((String (*)(B2)) &__A::toString),
+        hashCode((int32_t (*)(B2)) &__Object::hashCode),
+        getClass((Class (*)(B2)) &__Object::getClass),
+        equals((bool (*)(B2, Object)) &__Object::equals),
+        goodbye(&__B2::goodbye),
+        helloJohn(&__B2::helloJohn),
+        helloWorld(&__B2::helloWorld) {}
     };
 
     struct __C;
@@ -103,7 +146,10 @@ namespace inputs {
       
       __C();
 
-      static void m_C_C(C,C,C);
+      String c;
+
+      static void helloDave(C);
+      static String toString(C);
       static C __init();
       static Class __class();
 
@@ -113,60 +159,64 @@ namespace inputs {
     struct __C_VT { 
       Class __is_a;
 
-      void (*m_Object_Object) (C);
-      void (*m_A_Object) (C);
-      void (*m_Object_A) (C);
+      void (*goodbye) (C);
+      void (*helloWorld) (C);
+      void (*helloJohn) (C);
+      void (*setA_String) (C);
+      void (*printOther_A) (C);
       int32_t (*hashCode) (C);
       Class (*getClass) (C);
-      String (*toString) (C);
       bool (*equals) (C, Object);
-      void (*m_C_C) (C);
+      void (*helloDave) (C);
+      String (*toString) (C);
 
       __C_VT()
       : __is_a(__C::__class()),
-        m_Object_Object((void (*)(C)) &__A::m_Object_Object),
-        m_A_Object((void (*)(C)) &__A::m_A_Object),
-        m_Object_A((void (*)(C)) &__A::m_Object_A),
+        goodbye((void (*)(C)) &__B2::goodbye),
+        helloWorld((void (*)(C)) &__B2::helloWorld),
+        helloJohn((void (*)(C)) &__B2::helloJohn),
+        setA_String((void (*)(C)) &__A::setA_String),
+        printOther_A((void (*)(C)) &__A::printOther_A),
         hashCode((int32_t (*)(C)) &__Object::hashCode),
         getClass((Class (*)(C)) &__Object::getClass),
-        toString((String (*)(C)) &__Object::toString),
         equals((bool (*)(C, Object)) &__Object::equals),
-        m_C_C(&__C::m_C_C) {}
+        helloDave(&__C::helloDave),
+        toString(&__C::toString) {}
     };
 
-    struct __Test041;
-    struct __Test041_VT;
-    typedef __Test041* Test041;
+    struct __CustomTest1;
+    struct __CustomTest1_VT;
+    typedef __CustomTest1* CustomTest1;
     
-    struct __Test041 { 
+    struct __CustomTest1 { 
       
-      __Test041_VT* __vptr;
+      __CustomTest1_VT* __vptr;
       
-      __Test041();
+      __CustomTest1();
 
-      static void main_String(Test041,String);
-      static Test041 __init();
+      static void main_String(CustomTest1,String);
+      static CustomTest1 __init();
       static Class __class();
 
-      static __Test041_VT __vtable;
+      static __CustomTest1_VT __vtable;
     };
 
-    struct __Test041_VT { 
+    struct __CustomTest1_VT { 
       Class __is_a;
 
-      int32_t (*hashCode) (Test041);
-      Class (*getClass) (Test041);
-      String (*toString) (Test041);
-      bool (*equals) (Test041, Object);
-      void (*main_String) (Test041);
+      int32_t (*hashCode) (CustomTest1);
+      Class (*getClass) (CustomTest1);
+      String (*toString) (CustomTest1);
+      bool (*equals) (CustomTest1, Object);
+      void (*main_String) (CustomTest1);
 
-      __Test041_VT()
-      : __is_a(__Test041::__class()),
-        hashCode((int32_t (*)(Test041)) &__Object::hashCode),
-        getClass((Class (*)(Test041)) &__Object::getClass),
-        toString((String (*)(Test041)) &__Object::toString),
-        equals((bool (*)(Test041, Object)) &__Object::equals),
-        main_String(&__Test041::main_String) {}
+      __CustomTest1_VT()
+      : __is_a(__CustomTest1::__class()),
+        hashCode((int32_t (*)(CustomTest1)) &__Object::hashCode),
+        getClass((Class (*)(CustomTest1)) &__Object::getClass),
+        toString((String (*)(CustomTest1)) &__Object::toString),
+        equals((bool (*)(CustomTest1, Object)) &__Object::equals),
+        main_String(&__CustomTest1::main_String) {}
     };
 
   }
--- src/main/java/edu/nyu/oop/CppDataLayoutM.java
+++ src/main/java/edu/nyu/oop/CppDataLayoutM.java
@@ -13,6 +13,7 @@ public class CppDataLayoutM {
     public static ArrayList<CppDataLayoutM.cppImplementationClass> implementationClasses;
 
 
+
     public CppDataLayoutM(){
         //Use this default constructor to create the deafult __Object data layout
         implementationClasses = new ArrayList<cppImplementationClass>();
@@ -20,91 +21,322 @@ public class CppDataLayoutM {
     }
 
     public static class cppImplementationClass{
-     String className;
-     String vTableInit;
-     String classMethodInit;
-     String vTableDecl;
-     String parentClass;
+        String className;
+        String vTableInit;
+        String classMethodInit;
+        String vTableDecl;
+        String parentClass;
 
-     ArrayList<cppMethodImplementation> cppMethodImplementations;
+        ArrayList<cppMethodImplementation> cppMethodImplementations;
 
-     public cppImplementationClass (CustomClassObject theClass){
-      if (theClass.getParentClass() == "None") {
-          this.parentClass = "Object";
-      } else {
-          this.parentClass = theClass.getParentClass();
-      }
-      this.className = theClass.getClassName();
-      this.vTableInit = "__" + this.className + "::__" + this.className +  "() : __vptr(&__vtable) {}";
-      this.classMethodInit = "Class __" + this.className + "::__class() {\n" +
-              "      static Class k =\n" +
-              "        new __Class(__rt::literal(\"inputs.javalang." + this.className + "\"), __" + parentClass + "::__class());\n" +
-              "      return k;\n" +
-              "    }";
-      this.vTableDecl = "__" + this.className + "_VT " + "__" + this.className + "::__vtable";
-      this.cppMethodImplementations = new ArrayList<cppMethodImplementation>();
+        public cppImplementationClass (CustomClassObject theClass){
+            if (theClass.getParentClass() == "None") {
+                this.parentClass = "Object";
+            } else {
+                this.parentClass = theClass.getParentClass();
+            }
+            this.className = theClass.getClassName();
+            this.vTableInit = "__" + this.className + "::__" + this.className +  "() : __vptr(&__vtable) {}";
+            this.classMethodInit = "Class __" + this.className + "::__class() {\n" +
+                    "      static Class k =\n" +
+                    "        new __Class(__rt::literal(\"inputs.javalang." + this.className + "\"), __" + parentClass + "::__class());\n" +
+                    "      return k;\n" +
+                    "    }";
+            this.vTableDecl = "__" + this.className + "_VT " + "__" + this.className + "::__vtable";
+            this.cppMethodImplementations = new ArrayList<cppMethodImplementation>();
 
-         System.out.println("CLASS NAME");
-         System.out.println(this.className);
-         System.out.println("CLASS VTABLE INIT");
-         System.out.println(this.vTableInit);
-         System.out.println("CLASS METHOD INIT");
-         System.out.println(this.classMethodInit);
-         System.out.println("CLASS VTABLE DECL");
-         System.out.println(this.vTableDecl);
-
-
-      for (CustomMethodClass method : theClass.getMethods()){
-          cppMethodImplementation methodImp = new cppMethodImplementation(method, this.className);
-          this.cppMethodImplementations.add(methodImp);
-      }
-
-     }
-
-     //TODO
-     private static class cppMethodImplementation{
-         String returnType;
-         String name;
-         String params;
-
-         Node theBlock;
-
-         public cppMethodImplementation(CustomMethodClass methodClass, String className){
-             CppDataLayout.typeTranslate typeTranslate = new CppDataLayout.typeTranslate();
-             this.returnType = typeTranslate.translateType(methodClass.getReturnType());
-             // name = __className
-             this.name = "__" + className + "::" + methodClass.getName();
-             // TODO: what if its static?
-             this.params = "(" + className + " __this";
-             for (CustomVariablesClass var : methodClass.getParameters()) {
-                 this.params += ", " + var.type + " " + var.name;
-             }
-             this.params += ")";
-             this.theBlock = methodClass.getMethodsBlock();
-
-             System.out.println("METHOD NAME");
-             System.out.println(this.name);
-
-             System.out.println("METHOD PARAMS");
-             System.out.println(this.params);
-
-             System.out.println("METHOD RETURN");
-             System.out.println(this.returnType);
-
-             System.out.println("THE BLOCK");
-             System.out.println(this.theBlock);
+            System.out.println("CLASS NAME");
+            System.out.println(this.className);
+            System.out.println("CLASS VTABLE INIT");
+            System.out.println(this.vTableInit);
+            System.out.println("CLASS METHOD INIT");
+            System.out.println(this.classMethodInit);
+            System.out.println("CLASS VTABLE DECL");
+            System.out.println(this.vTableDecl);
 
 
+            for (CustomMethodClass method : theClass.getMethods()){
+                cppMethodImplementation methodImp = new cppMethodImplementation(method, this.className);
+                this.cppMethodImplementations.add(methodImp);
+            }
+
+        }
+
+        public static class CustomFieldDeclaration{
+
+            int position;
+            ArrayList<String> modifiers;
+            String Type;
+            ArrayList<String> declarators;
+            ArrayList<String> arguments;
+
+            public CustomFieldDeclaration(Node fieldDec, int position){
+                modifiers =new ArrayList<String>();
+                declarators = new ArrayList<String>();
+                arguments =new ArrayList<String>();
+                this.position = position;
+
+                // for (int i = 0; i < fieldDec.size(); i++){
+
+                // get all indices of field declaration and translate
+                // 0: modifiers
+                // 1: Type(QualifiedIdentifier("string value"),
+                // 2: Declarators(0:Declarator("dec variable"), 1:someVal/null, 2:NewClassExpression,
+                // 3: Arguments("some string/null"), someVal/null)
+
+                // 0: modifiers
+                if (fieldDec.getNode(0).size() > 0 ){
+
+                    for (int modifierIndex = 0; modifierIndex < fieldDec.getNode(0).size(); modifierIndex++){
+                        String getModifier = fieldDec.getNode(0).getString(modifierIndex);
+
+                        modifiers.add(getModifier);
+
+                    }
 
 
-         }
+                } // end check modifiers
 
-     }
+                // 1: Type(QualifiedIdentifier("string value"),
+                if (fieldDec.getNode(1).size() > 0 ) {
+
+                    System.out.println("Get Type");
+                    System.out.println((fieldDec.getNode(1)));
+                    String fieldDeclarationLine ="";
+                    String qualifiedIdentifier;
+                    for (int j = 0; j < fieldDec.getNode(1).size(); j++){
+
+                        // if value of node is null continue
+                        if (fieldDec.getNode(1).getNode(j) == null){continue;}
+                        // sub 0th node is Qualified Identifier
+                        if (fieldDec.getNode(1).getNode(j).getName().equals("QualifiedIdentifier")){
+
+                            System.out.println("QI\n" + fieldDec.getNode(1).getNode(j));
+                            System.out.println("QIVal\n" + fieldDec.getNode(1).getNode(j).getString(0));
+
+//                                if (fieldDec.getNode(1).getNode(j).getString(0) == null){continue;}
+                            qualifiedIdentifier = fieldDec.getNode(1).getNode(j).getString(0);
+                            fieldDeclarationLine += qualifiedIdentifier;
+                        }
 
 
+
+
+
+
+                        System.out.println("types vals " + fieldDec.getNode(1).getNode(j));
+
+
+                    }
+
+
+
+                } // end of sub 0 check field dec's modifiers
+
+                // check field dec's declarators
+                String declaratorValue = "";
+
+
+                Node declarators = fieldDec.getNode(2);
+
+                if (declarators.size() > 0 ) {
+                    System.out.println("DECLARATORS1 ");
+
+                    System.out.println(declarators);
+                    Node declarator = declarators.getNode(0);
+
+                    if (!declarator.isEmpty()){
+
+
+                        String declaratorVar = declarator.getString(0);
+
+                        // if declator instantiates an object
+                        // must get the rhs
+                        if (declarator.getNode(2).getName().equals("NewClassExpression")){
+                            declaratorValue = "new";
+                            Node newClassExpression = declarator.getNode(2);
+                            declaratorValue += newClassExpression.getNode(2).getString(0);
+
+                            // check if the instantiated object has arguments
+                            if (!newClassExpression.getNode(3).isEmpty()){
+
+                                Node arguments = newClassExpression.getNode(3);
+                                System.out.println("(");
+                                for (int x = 0; x < arguments.size(); x++) {
+                                    declaratorValue += arguments.getNode(x).getString(0);
+
+                                    if (x < arguments.size()-1){
+                                        declaratorValue += ",";
+
+                                    }
+
+                                }
+                                System.out.println(")");
+
+
+                            }else{
+                                declaratorValue += "()";
+                            }
+                            // end of check if instantiated object has arguments
+
+
+                        } // end of if NewClassExpression
+
+
+
+
+
+                    } // end of if declator isn't empty
+
+
+
+
+
+
+
+
+
+
+
+
+                } // end of get Declarators
+
+
+
+
+
+            } // end of CustomFieldDeclaration Constructor
+
+
+        }
+
+        public static class TranslatedBlock{
+            // blocks have field declarations
+            // Expression statements
+            ArrayList<CustomFieldDeclaration> fieldDeclarations;
+
+
+            public TranslatedBlock(Node b){
+
+                fieldDeclarations = new ArrayList<CustomFieldDeclaration>();
+                System.out.println(  b.size());
+
+                for (int i =0; i < b.size(); i++){
+
+                    // either a field declaration or ExpressionStatement
+
+                    // if field declaration
+                    // its subnodes have the following indices:
+                    // 0: modifiers
+                    // 1: Type(QualifiedIdentifier("string value"),
+                    // 2: Declarators(0:Declarator("dec variable"), 1:someVal/null, 2:NewClassExpression,
+                    // 3: Arguments("some string/null"), someVal/null)
+
+                    if (b.getNode(i).getName().equals("FieldDeclaration")){
+                        CustomFieldDeclaration fd = new CustomFieldDeclaration(b.getNode(i), i);
+                        this.fieldDeclarations.add(fd);
+
+                    }
+                    // else if ExpressionStatement
+                    // its subnodes have the following indices
+                    // 0:
+                    else{
+                        System.out.println("Expression Statement\n " + b.getNode(i));
+                    }
+
+
+
+
+
+
+                }
+
+            }
+
+        }
+
+        //TODO
+        private static class cppMethodImplementation{
+            String returnType;
+            String name;
+            String params;
+
+            Node theBlock;
+
+            // TODO: add property to hold translated block, check if the block has a constructor
+
+            boolean isConstuctor = false;
+
+
+            //blockimpclass [= ]
+
+            public cppMethodImplementation(CustomMethodClass methodClass, String className){
+                CppDataLayout.typeTranslate typeTranslate = new CppDataLayout.typeTranslate();
+                this.returnType = typeTranslate.translateType(methodClass.getReturnType());
+                // name = __className
+                this.name = "__" + className + "::" + methodClass.getName();
+                // TODO: what if its static?
+                this.params = "(" + className + " __this";
+                for (CustomVariablesClass var : methodClass.getParameters()) {
+                    this.params += ", " + var.type + " " + var.name;
+                }
+                this.params += ")";
+                this.theBlock = methodClass.getMethodsBlock();
+
+                //Check if this
+                if(this.name.contains("init")){
+                    this.isConstuctor = true;
+                }
+
+                System.out.println("METHOD NAME");
+                System.out.println(this.name);
+
+                System.out.println("METHOD PARAMS");
+                System.out.println(this.params);
+
+                System.out.println("METHOD RETURN");
+                System.out.println(this.returnType);
+
+                System.out.println("THE BLOCK");
+                System.out.println(this.theBlock);
+                System.out.println("block12345");
+
+                TranslatedBlock translatedBlock = new TranslatedBlock(theBlock);
+
+
+
+
+
+
+//                 System.out.println("start i");
+//                 System.out.print();
+//
+//                for (int j = 0; j < this.theBlock.getNode(i).size(); j++){
+//
+//                    System.out.println("in j block " + this.theBlock.getNode(i).getNode(j) +"\n");
+//
+//
+//
+//                 }
+//                 System.out.println("inc i");
+//
+//
+//
+//             }
+
+                // System.out.println()
+
+                // TODO: translate the block
+
+
+            }
+
+        }
+        // A a = new A();
 
 
 
 
     }
 }
+
