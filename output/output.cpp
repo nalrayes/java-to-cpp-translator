969817ceb06d44576f3745f6179d37cfe623bf3e
charleswdickstein <charliewoodrowdickstein@gmail.com>
2017-12-11 18:27:07 -0500

    added for loop header

--- output/output.cpp
+++ output/output.cpp
@@ -9,37 +9,7 @@ using namespace inputs::constructors;
 using namespace namespace std;
 
 namespace inputs {
-  namespace test008 {
+  namespace test022 {
 
-    __A::__A() : __vptr(&__vtable), a(null)
-    {}
-
-    Class __A::__class() {
-      static Class k =
-        new __Class(__rt::literal("inputs.javalang.A"), __Object::__class());
-      return k;
-    }
-
-    __A_VT __A::__vtable
-
-    A __A::__init(A __this) {
-
-    }
-
-      __B::__B() : __vptr(&__vtable), b(null)
-    {}
-
-      Class __B::__class() {
-      static Class k =
-        new __Class(__rt::literal("inputs.javalang.B"), __A::__class());
-      return k;
-    }
-
-      __B_VT __B::__vtable
-
-      B __B::__init(B __this) {
-
-      }
-
-    }
-  }
+}
+}
--- output/output.h
+++ output/output.h
@@ -5,108 +5,40 @@
 using namespace java::lang;
 
 namespace inputs {
-  namespace test008 {
+  namespace test022 {
   
-    struct __A;
-    struct __A_VT;
-    typedef __A* A;
+    struct __Test022;
+    struct __Test022_VT;
+    typedef __Test022* Test022;
     
-    struct __A { 
+    struct __Test022 { 
       
-      __A_VT* __vptr;
+      __Test022_VT* __vptr;
       
-      __A();
+      __Test022();
 
-      String a;
-
-      static A __init();
+      static void main_String(Test022,String);
+      static Test022 __init();
       static Class __class();
 
-      static __A_VT __vtable;
+      static __Test022_VT __vtable;
     };
 
-    struct __A_VT { 
+    struct __Test022_VT { 
       Class __is_a;
 
-      int32_t (*hashCode) (A);
-      Class (*getClass) (A);
-      String (*toString) (A);
-      bool (*equals) (A, Object);
+      int32_t (*hashCode) (Test022);
+      Class (*getClass) (Test022);
+      String (*toString) (Test022);
+      bool (*equals) (Test022, Object);
 
-      __A_VT()
-      : __is_a(__A::__class()),
-        hashCode((int32_t (*)(A)) &__Object::hashCode),
-        getClass((Class (*)(A)) &__Object::getClass),
-        toString((String (*)(A)) &__Object::toString),
-        equals((bool (*)(A, Object)) &__Object::equals) {}
-    };
-
-    struct __B;
-    struct __B_VT;
-    typedef __B* B;
-    
-    struct __B { 
-      
-      __B_VT* __vptr;
-      
-      __B();
-
-      String b;
-
-      static B __init();
-      static Class __class();
-
-      static __B_VT __vtable;
-    };
-
-    struct __B_VT { 
-      Class __is_a;
-
-      int32_t (*hashCode) (B);
-      Class (*getClass) (B);
-      String (*toString) (B);
-      bool (*equals) (B, Object);
-
-      __B_VT()
-      : __is_a(__B::__class()),
-        hashCode((int32_t (*)(B)) &__Object::hashCode),
-        getClass((Class (*)(B)) &__Object::getClass),
-        toString((String (*)(B)) &__Object::toString),
-        equals((bool (*)(B, Object)) &__Object::equals) {}
-    };
-
-    struct __Test008;
-    struct __Test008_VT;
-    typedef __Test008* Test008;
-    
-    struct __Test008 { 
-      
-      __Test008_VT* __vptr;
-      
-      __Test008();
-
-      static void main_String(Test008,String);
-      static Test008 __init();
-      static Class __class();
-
-      static __Test008_VT __vtable;
-    };
-
-    struct __Test008_VT { 
-      Class __is_a;
-
-      int32_t (*hashCode) (Test008);
-      Class (*getClass) (Test008);
-      String (*toString) (Test008);
-      bool (*equals) (Test008, Object);
-
-      __Test008_VT()
-      : __is_a(__Test008::__class()),
-        hashCode((int32_t (*)(Test008)) &__Object::hashCode),
-        getClass((Class (*)(Test008)) &__Object::getClass),
-        toString((String (*)(Test008)) &__Object::toString),
-        equals((bool (*)(Test008, Object)) &__Object::equals),
-        main_String(&__Test008::main_String) {}
+      __Test022_VT()
+      : __is_a(__Test022::__class()),
+        hashCode((int32_t (*)(Test022)) &__Object::hashCode),
+        getClass((Class (*)(Test022)) &__Object::getClass),
+        toString((String (*)(Test022)) &__Object::toString),
+        equals((bool (*)(Test022, Object)) &__Object::equals),
+        main_String(&__Test022::main_String) {}
     };
 
   }
