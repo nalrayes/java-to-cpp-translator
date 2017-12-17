e7770258b1ab0a937121bd8dacb842df48572fbf
nalrayes <thenalrayes@gmail.com>
2017-12-17 16:08:33 -0500

    pulling

--- output/output.cpp
+++ output/output.cpp
@@ -5,15 +5,9 @@ using namespace java::lang;
 using namespace std;
 
 namespace inputs {
-<<<<<<< HEAD
   namespace test006 {
 
     __A::__A() : __vptr(&__vtable), fld(__rt::null())
-=======
-  namespace test005 {
-
-    __A::__A() : __vptr(&__vtable)
->>>>>>> 8c001fd88a280e6bc77ec4974377399f11358cdf
     {}
 
     Class __A::__class() {
@@ -26,7 +20,6 @@ namespace inputs {
 
     A __A::__init(A __this) {
       __Object::__init((Object) __this);
-<<<<<<< HEAD
       __this->fld = __rt::literal("A");
       return __this;
     }
@@ -42,33 +35,6 @@ namespace inputs {
     
     String __A::getFld(A __this) {
       return __this->fld;
-=======
-      return __this;
-    }
-    
-    String __A::toString(A __this) {
-      return __rt::literal("A");
-    }
-    
-    __B::__B() : __vptr(&__vtable)
-    {}
-
-    Class __B::__class() {
-      static Class k =
-        new __Class(__rt::literal("inputs.javalang.B"), __A::__class());
-      return k;
-    }
-
-    __B_VT __B::__vtable;
-
-    B __B::__init(B __this) {
-      __A::__init((A) __this);
-      return __this;
-    }
-    
-    String __B::toString(B __this) {
-      return __rt::literal("B");
->>>>>>> 8c001fd88a280e6bc77ec4974377399f11358cdf
     }
     
   }
--- output/output.h
+++ output/output.h
@@ -5,11 +5,7 @@
 using namespace java::lang;
 
 namespace inputs {
-<<<<<<< HEAD
   namespace test006 {
-=======
-  namespace test005 {
->>>>>>> 8c001fd88a280e6bc77ec4974377399f11358cdf
   
     struct __A;
     struct __A_VT;
@@ -56,7 +52,6 @@ namespace inputs {
         getFld(&__A::getFld) {}
     };
 
-<<<<<<< HEAD
     struct __Test006;
     struct __Test006_VT;
     typedef __rt::Ptr<__Test006> Test006;
@@ -92,78 +87,6 @@ namespace inputs {
         getClass((Class (*)(Test006)) &__Object::getClass),
         toString((String (*)(Test006)) &__Object::toString),
         main_String(&__Test006::main_String) {}
-=======
-    struct __B;
-    struct __B_VT;
-    typedef __rt::Ptr<__B> B;
-    
-    struct __B { 
-      
-      __B_VT* __vptr;
-      
-      __B();
-
-      static String toString(B);
-      static B __init(B __this);
-      static Class __class();
-
-      static __B_VT __vtable;
-    };
-
-    struct __B_VT { 
-      Class __is_a;
-
-      void (*__delete) (__B*);
-      int32_t (*hashCode) (B);
-      bool (*equals) (B, Object);
-      Class (*getClass) (B);
-      String (*toString) (B);
-
-      __B_VT()
-      : __is_a(__B::__class()),
-        __delete(__rt::__delete<__B>),
-        hashCode((int32_t (*)(B)) &__Object::hashCode),
-        equals((bool (*)(B, Object)) &__Object::equals),
-        getClass((Class (*)(B)) &__Object::getClass),
-        toString(&__B::toString) {}
-    };
-
-    struct __Test005;
-    struct __Test005_VT;
-    typedef __rt::Ptr<__Test005> Test005;
-    
-    struct __Test005 { 
-      
-      __Test005_VT* __vptr;
-      
-      __Test005();
-
-      static void main_String(Test005, String);
-      static Test005 __init(Test005 __this);
-      static Class __class();
-
-      static __Test005_VT __vtable;
-    };
-
-    struct __Test005_VT { 
-      Class __is_a;
-
-      void (*__delete) (__Test005*);
-      int32_t (*hashCode) (Test005);
-      bool (*equals) (Test005, Object);
-      Class (*getClass) (Test005);
-      String (*toString) (Test005);
-      void (*main_String) (Test005, String);
-
-      __Test005_VT()
-      : __is_a(__Test005::__class()),
-        __delete(__rt::__delete<__Test005>),
-        hashCode((int32_t (*)(Test005)) &__Object::hashCode),
-        equals((bool (*)(Test005, Object)) &__Object::equals),
-        getClass((Class (*)(Test005)) &__Object::getClass),
-        toString((String (*)(Test005)) &__Object::toString),
-        main_String(&__Test005::main_String) {}
->>>>>>> 8c001fd88a280e6bc77ec4974377399f11358cdf
     };
 
   }
