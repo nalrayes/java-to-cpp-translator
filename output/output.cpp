64f0ea38dbc6552b41eecfa9c99b85aee520ef42
charleswdickstein <charliewoodrowdickstein@gmail.com>
2017-12-18 04:28:01 -0500

    static in main need to put declaration outside

--- output/output.cpp
+++ output/output.cpp
@@ -5,9 +5,9 @@ using namespace java::lang;
 using namespace std;
 
 namespace inputs {
-  namespace test010 {
+  namespace test019 {
 
-    __A::__A() : __vptr(&__vtable), a(__rt::null())
+    __A::__A() : __vptr(&__vtable)
     {}
 
     Class __A::__class() {
@@ -20,66 +20,6 @@ namespace inputs {
 
     A __A::__init(A __this) {
       __Object::__init((Object) __this);
-      return __this;
-    }
-    
-    void __A::setA_String(A __this, String x) {
-      __this->a = x;
-    }
-    
-    void __A::printOther_A(A __this, A other) {
-      std::cout << ({__rt::checkNotNull(other); other->__vptr->toString(other);}) << std::endl;
-    }
-    
-    String __A::toString(A __this) {
-      return __this->a;
-    }
-    
-    __B1::__B1() : __vptr(&__vtable), b(__rt::null())
-    {}
-
-    Class __B1::__class() {
-      static Class k =
-        new __Class(__rt::literal("inputs.javalang.B1"), __A::__class());
-      return k;
-    }
-
-    __B1_VT __B1::__vtable;
-
-    B1 __B1::__init(B1 __this) {
-      __A::__init((A) __this);
-      return __this;
-    }
-    
-    __B2::__B2() : __vptr(&__vtable), b(__rt::null())
-    {}
-
-    Class __B2::__class() {
-      static Class k =
-        new __Class(__rt::literal("inputs.javalang.B2"), __A::__class());
-      return k;
-    }
-
-    __B2_VT __B2::__vtable;
-
-    B2 __B2::__init(B2 __this) {
-      __A::__init((A) __this);
-      return __this;
-    }
-    
-    __C::__C() : __vptr(&__vtable), c(__rt::null())
-    {}
-
-    Class __C::__class() {
-      static Class k =
-        new __Class(__rt::literal("inputs.javalang.C"), __B1::__class());
-      return k;
-    }
-
-    __C_VT __C::__vtable;
-
-    C __C::__init(C __this) {
-      __B1::__init((B1) __this);
       return __this;
     }
     
